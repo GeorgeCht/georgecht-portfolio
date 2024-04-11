571a81521579a147ce44e913e10f707ba335f044
GeorgeCht <2hardforutoo@hotmail.gr>
2024-04-11 11:05:20 +0300

    Fixed navigation test

--- src/tests/navigation.spec.ts
+++ src/tests/navigation.spec.ts
@@ -4,9 +4,25 @@ import { test, expect } from '@playwright/test'
 test('main navigation', async ({ page }) => {
   await page.goto('/')
   await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight))
-  await page.getByRole('button', { name: includes('Navigate') }).click()
-  await expect(page.getByRole('navigation')).toContainText('Projects')
-  await expect(page.getByRole('navigation')).toContainText('About')
-  await expect(page.getByRole('navigation')).toContainText('Archive')
-  await expect(page.getByRole('navigation')).toContainText('Contact')
+
+  await expect(
+    page.getByRole('link', { name: 'projects projects' }),
+  ).toBeVisible()
+  await expect(page.getByRole('banner')).toContainText('projects')
+
+  await expect(page.getByRole('link', { name: 'about about' })).toBeVisible()
+  await expect(page.getByRole('banner')).toContainText('about')
+
+  await expect(
+    page.getByRole('link', { name: 'archive archive' }),
+  ).toBeVisible()
+  await expect(page.getByRole('banner')).toContainText('archive')
+
+  await expect(
+    page.getByRole('link', { name: 'contact contact' }),
+  ).toBeVisible()
+  await expect(page.getByRole('banner')).toContainText('contact')
+
+  await expect(page.getByRole('link', { name: 'cv cv' })).toBeVisible()
+  await expect(page.getByRole('banner')).toContainText('cv')
 })
